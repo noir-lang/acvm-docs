cc7b6caf5007812d77e9263de0de59104c582054
Josh Crites <jc@joshcrites.com>
2023-04-13 21:49:11 -0400

    disable trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,6 +12,8 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
+  trailingSlash: false,
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
